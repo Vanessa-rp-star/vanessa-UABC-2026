0f8353b8ac8ba451e0e53f8d9e17a3b67f1714d7
Vanessa-rp-star <vanessa.perez10@uabc.edu.mx>
2026-05-01 09:46:39 -0700

    Update test.py

--- test/test.py
+++ test/test.py
@@ -1,6 +1,5 @@
 # SPDX-FileCopyrightText: © 2024 Tiny Tapeout
 # SPDX-License-Identifier: Apache-2.0
-
 import cocotb
 from cocotb.clock import Clock
 from cocotb.triggers import ClockCycles
@@ -8,26 +7,27 @@ from cocotb.triggers import ClockCycles
 
 @cocotb.test()
 async def test_project(dut):
-    dut._log.info("Start")
 
     clock = Clock(dut.clk, 10, unit="us")
     cocotb.start_soon(clock.start())
 
-    # Reset
     dut.ena.value = 1
-    dut.ui_in.value = 0
-    dut.uio_in.value = 0
-    dut.rst_n.value = 0
-
-    await ClockCycles(dut.clk, 10)
     dut.rst_n.value = 1
 
-    dut._log.info("Running CPU...")
+    # Test ADD: 3 + 2 = 5
+    dut.ui_in.value = (2 << 4) | 3   # B=2, A=3
+    dut.uio_in.value = 0             # ADD
+    await ClockCycles(dut.clk, 1)
+    assert dut.uo_out.value == 5
 
-    # Deja correr varios ciclos para que la CPU haga algo
-    await ClockCycles(dut.clk, 20)
+    # Test AND: 3 & 1 = 1
+    dut.ui_in.value = (1 << 4) | 3
+    dut.uio_in.value = 2
+    await ClockCycles(dut.clk, 1)
+    assert dut.uo_out.value == 1
 
-    # Solo verificamos que NO sea X o Z
-    assert dut.uo_out.value.is_resolvable, "Salida inválida (X o Z)"
-
-    dut._log.info(f"Salida: {dut.uo_out.value}")
+    # Test OR: 3 | 4 = 7
+    dut.ui_in.value = (4 << 4) | 3
+    dut.uio_in.value = 3
+    await ClockCycles(dut.clk, 1)
+    assert dut.uo_out.value == 7
