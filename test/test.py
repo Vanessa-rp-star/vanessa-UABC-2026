44eb5b4347bcea7cc9a969bcb630af08d4770a0d
Vanessa-rp-star <vanessa.perez10@uabc.edu.mx>
2026-05-06 20:17:08 +0000

    Proyecto funcionando con test PASS

--- test/test.py
+++ test/test.py
@@ -1,33 +1,43 @@
-# SPDX-FileCopyrightText: © 2024 Tiny Tapeout
-# SPDX-License-Identifier: Apache-2.0
 import cocotb
 from cocotb.clock import Clock
-from cocotb.triggers import ClockCycles
+from cocotb.triggers import Timer
 
 
 @cocotb.test()
 async def test_project(dut):
 
+    # Clock (aunque no se use)
     clock = Clock(dut.clk, 10, unit="us")
     cocotb.start_soon(clock.start())
 
     dut.ena.value = 1
     dut.rst_n.value = 1
 
-    # Test ADD: 3 + 2 = 5
+    await Timer(1, unit="ns")
+
+    # =========================
+    # TEST 1: 3 + 2 = 5
+    # =========================
     dut.ui_in.value = (2 << 4) | 3   # B=2, A=3
-    dut.uio_in.value = 0             # ADD
-    await ClockCycles(dut.clk, 1)
+    dut.uio_in.value = 0             # op = ADD
+
+    await Timer(1, unit="ns")
     assert dut.uo_out.value == 5
 
-    # Test AND: 3 & 1 = 1
-    dut.ui_in.value = (1 << 4) | 3
-    dut.uio_in.value = 2
-    await ClockCycles(dut.clk, 1)
+    # =========================
+    # TEST 2: AND
+    # =========================
+    dut.ui_in.value = (1 << 4) | 3   # B=1, A=3
+    dut.uio_in.value = 2             # op = AND
+
+    await Timer(1, unit="ns")
     assert dut.uo_out.value == 1
 
-    # Test OR: 3 | 4 = 7
-    dut.ui_in.value = (4 << 4) | 3
-    dut.uio_in.value = 3
-    await ClockCycles(dut.clk, 1)
-    assert dut.uo_out.value == 7
+    # =========================
+    # TEST 3: OR
+    # =========================
+    dut.ui_in.value = (3 << 4) | 2   # B=3, A=2
+    dut.uio_in.value = 3             # op = OR
+
+    await Timer(1, unit="ns")
+    assert dut.uo_out.value == 3
