d31aeed0d1b19f64f939c2bbded2c39eb3c737c4
Vanessa-rp-star <vanessa.perez10@uabc.edu.mx>
2026-05-07 13:32:11 +0000

    Fix info.yaml

--- src/project.v
+++ src/project.v
@@ -19,16 +19,18 @@ module tt_um_example (
     wire [3:0] B = ui_in[7:4];
     wire [2:0] op = uio_in[2:0];
 
-    reg [7:0] result;
+    reg [7:0] result = 8'b0;
 
     always @(*) begin
+        result = 8'b0;
+
         case (op)
-            3'b000: result = A + B;                         // ADD
-            3'b001: result = A - B;                         // SUB
-            3'b010: result = A & B;                         // AND
-            3'b011: result = A | B;                         // OR
-            3'b100: result = A ^ B;                         // XOR
-            3'b101: result = ($signed(A) < $signed(B)) ? 1 : 0; // SLT
+            3'b000: result = {4'b0, A} + {4'b0, B}; // ADD
+            3'b001: result = {4'b0, A} - {4'b0, B}; // SUB
+            3'b010: result = {4'b0, A & B};         // AND
+            3'b011: result = {4'b0, A | B};         // OR
+            3'b100: result = {4'b0, A ^ B};         // XOR
+            3'b101: result = ($signed({1'b0,A}) < $signed({1'b0,B})) ? 8'd1 : 8'd0; // SLT
             default: result = 8'b0;
         endcase
     end
